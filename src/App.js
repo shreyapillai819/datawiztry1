8dae708c152b42d787cdf754d155179489b60224
shreyapillai819 <shreyapillai819@gmail.com>
2024-02-11 14:24:26 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import LoginForm from './login';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
