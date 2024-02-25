b5a879c2b203719ecae9198bd30e56fb4635161d
shreyapillai819 <132468495+shreyapillai819@users.noreply.github.com>
2024-02-25 04:48:06 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-// App.js
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // Custom CSS for styling
+
+import Navbar from './Navbar';
+import Footer from './Footer';
 import LoginForm from './login';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      <Navbar/>
+      <div className="container mt-5">
+        <LoginForm />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+// Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-light text-center py-4">
+      <div className="container">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
